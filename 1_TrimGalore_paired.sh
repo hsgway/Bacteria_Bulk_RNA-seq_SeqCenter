17e8a17712fd79bc99beb15c2f0e642efb3de8b7
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-10 00:19:48 -0500

    Update 1_TrimGalore_paired.sh

--- 1_TrimGalore_paired.sh
+++ 1_TrimGalore_paired.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # create a new directory for output files
-mkdir /Volumes/Expansion/Franz/FASTQ_trimmed
+mkdir /Volumes/Expansion/<your folder>/FASTQ_trimmed
 
 # directory
-dir="/Volumes/Expansion/Franz/RNAseq\ IO\ LB\ PolyB/OIpolyBFZ-372447203/FASTQ_Generation_2022-11-15_00_07_20Z-629512886"
-outdir="/Volumes/Expansion/Franz/FASTQ_trimmed"
+dir="/Volumes/Expansion/<your FASTQ folder>"
+outdir="/Volumes/Expansion/<your folder>/FASTQ_trimmed"
 
 # move to the directory containing FASTQ files
 cd $dir
