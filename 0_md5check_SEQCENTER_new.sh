23f17b8330402af852329be5d18dd31a1a85c7f7
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-10 00:18:46 -0500

    Update 0_md5check_SEQCENTER_new.sh

--- 0_md5check_SEQCENTER_new.sh
+++ 0_md5check_SEQCENTER_new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # file and directory
-dir="/Volumes/Expansion/Bacteria_RNAseq_reads/20230406_Ian_CR_anaerobe"
+dir="/Volumes/Expansion/Bacteria_RNAseq_reads/<your_FASTQ_folder>"
 
 # go to the folder containing your files
 cd $dir
